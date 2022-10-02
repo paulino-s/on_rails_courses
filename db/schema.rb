c1736e2ccfa7a3ff438300fdf52ed32829d9c827
paulino-s <paulinovasquez900@gmail.com>
2022-10-01 23:46:36 -0600

    add controller lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_040932) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_02_053355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_040932) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "lessons", force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+  end
+
+  add_foreign_key "lessons", "courses"
 end
